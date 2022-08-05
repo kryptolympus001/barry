ccbfe5436da24aa48306286cba9b0aed8310a674
kryptolympus001 <110574478+kryptolympus001@users.noreply.github.com>
2022-08-04 20:23:37 -0700

    Update ChatBoxActivity.java

--- app/src/main/java/com/example/aymen/androidchat/ChatBoxActivity.java
+++ app/src/main/java/com/example/aymen/androidchat/ChatBoxActivity.java
@@ -43,7 +43,7 @@ public class ChatBoxActivity extends AppCompatActivity {
         Nickname= (String)getIntent().getExtras().getString(MainActivity.NICKNAME);
         //connect you socket client to the server
         try {
-            socket = IO.socket("http://192.168.1.3:3000");
+            socket = IO.socket("http://192.168.43.201:3000");
             socket.connect();
             socket.emit("join", Nickname);
         } catch (URISyntaxException e) {
